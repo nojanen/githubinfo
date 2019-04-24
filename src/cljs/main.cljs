6b3a4fde86961fa7a39ca2e39472a6fcd7ab8798
Niko Ojanen <nojanen@gmail.com>
2019-04-24 20:22:24 +0300

    Add handling of response

--- src/cljs/main.cljs
+++ src/cljs/main.cljs
@@ -4,11 +4,6 @@
               [ajax.core :refer [GET POST]]))
 
 (def cached-owners (r/atom []))
-(def owner-repos (r/atom []))
-
-
-(defn handler [response]
-    (reset! cached-owners response))
 
 (defn repo-data-component [repo]
     [:ul
@@ -17,27 +12,37 @@
         [:li [:img {:src "/img/watcher.svg"}] (get repo "watchers_count")]
         [:li [:img {:src "/img/fork.svg"}] (get repo "forks_count")]
         [:li [:img {:src "/img/issue.svg"}] (get repo "open_issues_count")]])
-    
-(defn owners-component []
-    (let [get-cached-owners (fn [] (GET "/api?org=nojanen" :handler (fn [rsp] (reset! cached-owners rsp)) :response-format :json))]
-        (get-cached-owners)
+
+(defn owner-repo-component [owner-name]
+    (def owner-repos (r/atom []))
+    (let [get-owner-repos (fn [] (GET (str "/api?org=" owner-name) :handler (fn [rsp] (reset! owner-repos rsp)) 
+                                                                   :response-format :json))]
+        (get-owner-repos)
         (fn []
             [:div {:class "owner"
-                   :on-click #(swap! (:style (js/document.getElementById "owner_foo")) "display:block")}
-                   ;; "document.getElementById('owner_foo').style='display:block';"}
-                [:div "foo"]
+                :on-click #(swap! (:style (js/document.getElementById "owner_foo")) "display:block")}
+                ;; "document.getElementById('owner_foo').style='display:block';"}
+                [:div owner-name]
                 [:div {:class "repoList" :id "owner_foo" :align "center"}
-                    (doall (for [index (range (count @cached-owners))]
-                        ^{:key index} [repo-data-component (get @cached-owners index)]))]])))
+                    (doall (for [index (range (count @owner-repos))]
+                        ^{:key (str owner-name index)} [repo-data-component (get @owner-repos index)]))]])))
 
-(defn timer-component []
-    (let [seconds-elapsed (r/atom 0)]
+(defn owners-component []
+    (let [get-cached-owners (fn [] (GET "/api" :handler (fn [rsp] (reset! cached-owners rsp))
+                                               :response-format :json))]
+        (get-cached-owners)
         (fn []
-        (js/setTimeout #(swap! seconds-elapsed inc) 1000)
-        [:div
-            "Seconds Elapsed: " @seconds-elapsed])))
+            [:div
+            (doall (for [owner-index (range (count @cached-owners))]
+                ^{:key (str "owner" owner-index)} [owner-repo-component (get @cached-owners owner-index)]))])))
+
+(defn main-view []
+    [:div "Cache new org/user:"
+        [:input {:type "text"}]
+        [:input {:type "button" :value "Add" }]
+    [owners-component]])
 
 (defn ^:export run []
     (r/render 
-        [owners-component]
+        [main-view]
         (js/document.getElementById "app")))
